5223ad9a75040e7909559917b512f81fdea67392
Bruno <brunoaugusto@outlook.com.br>
2019-12-20 14:18:14 -0300

    Alterações simples no main

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from time import sleep
 
 
 def logo():
-    print("\n\n\n\t\t\t\t 1.0 B")
+    print("\n\n\n\t\t\t\t    1.0 B",)
     print("\t\t\t\tVERSAO PYTHON")
-    print("\n")
     print("\n\t######## ######## ########  ##     ## #### ##    ##    ###    ##\n")
     print("\t   ##    ##       ##     ## ###   ###  ##  ###   ##   ## ##   ##\n")
     print("\t   ##    ##       ##     ## #### ####  ##  ####  ##  ##   ##  ##\n")
@@ -17,7 +16,8 @@ def logo():
     sleep(1)
 
 logo()
-nome = str(input('\t\t\t################ LOGIN ####################\n')).strip()#elimina os espaços
-nome = str(input('\t\t\t################ SENHA ####################\n')).strip()#elimina os espaços
+
+TipoLogin = str(input('\t\t[1] USUARIO COMUM              [0] Administrador \n')).strip()#elimina os espaços
+
 
 
