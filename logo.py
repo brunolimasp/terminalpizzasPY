f1b9eaf2732f2a414474c4e8563b2d6b0f77d741
LEANDRO DA SILVA FERREIRA <leandrosf934@gmail.com>
2020-01-05 14:31:22 -0300

    Atulização de cadastro e funções novas no logo

--- logo.py
+++ logo.py
@@ -1,5 +1,5 @@
 import os
-
+import sys
 
 def logo():
     os.system("cls")
@@ -13,3 +13,25 @@ def logo():
     print("\t   ##    ##       ##    ##  ##     ##  ##  ##   ### ##     ## ##\n")
     print("\t   ##    ######## ##     ## ##     ## #### ##    ## ##     ## ########\n\n")
     print("\t >PIZZAS_\n\n\n")
+
+# Print de linhas para estetica do sistema
+def linha():
+    print(55 * '#')
+
+def limpeza():
+    import sys
+    # Pega qual é o SO
+    so = sys.platform
+    so_clear = ''
+
+    # Defini o argumento para limpeza de tela de acordo com o SO
+    if (so == 'linux'):
+        so_clear = 'clear'
+    elif (so[:3] == 'win'):
+        so_clear = 'cls'
+
+    # Função lambda para limpeza de tela
+    if so_clear:
+        limpar = lambda l: os.system(l)
+    else:
+        limpar = lambda l: l
